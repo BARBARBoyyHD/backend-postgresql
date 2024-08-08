c5f59c34efd9638b1d5ffd43e418730612a3c3de
unknown <helyxrulk@gmail.com>
2024-08-09 01:40:32 +0700

    eqweqwe312556

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get("/api", async (req, res) => {
   }
 });
 
-
 // add items
 app.post("/api", async (req, res) => {
   try {
@@ -36,26 +35,48 @@ app.post("/api", async (req, res) => {
 
 // get single todo
 app.get("/api/:id", async (req, res) => {
-    try {
-        const {id}=req.params
-        const findItem = await pool.query("SELECT * FROM item_list WHERE item_id = $1", [id]);
-        res.json(findItem.rows[0]);
-    } catch (error) {   
-        res.json({ error: error.message });
-        res.status(404).json("Item not found");
-    }
-})
+  try {
+    const { id } = req.params;
+    const findItem = await pool.query(
+      "SELECT * FROM item_list WHERE item_id = $1",
+      [id]
+    );
+    res.json(findItem.rows[0]);
+  } catch (error) {
+    res.json({ error: error.message });
+    res.status(404).json("Item not found");
+  }
+});
 
 // delete items
 app.delete("/api/:id", async (req, res) => {
-    try {
-        const {id} = req.params
-        const deleteItems = await pool.query("DELETE FROM item_list WHERE item_id = $1", [id]);
-        res.json("Item deleted successfully");
-    } catch (error) {
-        console.log(error);
-    }
-})
+  try {
+    const { id } = req.params;
+    const deleteItems = await pool.query(
+      "DELETE FROM item_list WHERE item_id = $1",
+      [id]
+    );
+    res.json("Item deleted successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Edit items
+app.put("/api/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { item_name, item_description, item_price, item_quantity } = req.body;
+    await pool.query(
+      "UPDATE item_list SET item_name = $1, item_description = $2, item_price = $3, item_quantity = $4 WHERE item_id = $5",
+      [item_name, item_description, item_price, item_quantity, id]
+    );
+    res.status(200).json("item was updated successfully");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("An error occurred while updating the items");
+  }
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
